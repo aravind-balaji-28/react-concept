0fed7f432c20ab1ac5451d05c079c86eeda59e93
Aravind Balaji <aravindbalaji@solutionchamps.com>
2025-11-15 02:05:04 +0530

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,31 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    // uncomment and adjust if you want type-aware rules (requires tsconfig.json)
+    // project: "./tsconfig.json",
   },
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // for React projects use:
+    // "plugin:react/recommended",
+    // "plugin:react-hooks/recommended",
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking" // needs parserOptions.project
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+  ignorePatterns: ['dist/', 'build/', 'node_modules/'],
 };
--- src/views/ReactHooks/useReducer/index.tsx
+++ src/views/ReactHooks/useReducer/index.tsx
@@ -3,7 +3,7 @@
 // useReducer is a React Hook that lets you manage complex state using a reducer function.
 
 import { useReducer } from 'react';
-const main = 112;
+const main = 1127;
 const reducer = (state: any, action: any) => {
   switch (action.type) {
     case 'increment':
